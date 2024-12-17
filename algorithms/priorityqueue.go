4ca55d3eaa277c467eacee5cf7cedb41f345241c
Shawn Eddy <ymiseddy@gmail.com>
2024-12-16 21:46:47 -0500

    Priority queue now forces ints for costs.
    - Using underlying container/heap for the heap.
    - Using ints for costs.
    - Adding a minimum cost and maximum cost constructors.

--- algorithms/priorityqueue.go
+++ algorithms/priorityqueue.go
@@ -1,129 +1,178 @@
 package algorithms
 
 import (
+	"container/heap"
 	"errors"
-	"golang.org/x/exp/constraints"
+	"fmt"
+	"strings"
 )
 
-type priorityQueueNode[T comparable, K constraints.Ordered] struct {
-	priority K
-	data     T
+type priorityQueueNode[T comparable] struct {
+	Priority int
+	Data     T
+	Index    int
 }
 
-type PriorityQueue[T comparable, K constraints.Ordered] struct {
-	data []*priorityQueueNode[T, K]
+type PriorityQueue[T comparable] struct {
+	elements []*priorityQueueNode[T]
+	compFunc func(a, b int) bool
+}
+
+func NewMinCostPriorityQueue[T comparable](capacity int) *PriorityQueue[T] {
+	return newPriorityQueue[T](capacity, false)
+}
+
+func NewMaxCostPriorityQueue[T comparable](capacity int) *PriorityQueue[T] {
+	return newPriorityQueue[T](capacity, true)
 }
 
 // Create a new priority queue.
-func NewPriorityQueue[T comparable, K constraints.Ordered](capacity int) PriorityQueue[T, K] {
-	return PriorityQueue[T, K]{
-		data: make([]*priorityQueueNode[T, K], 0, capacity),
+func newPriorityQueue[T comparable](capacity int, maximize bool) *PriorityQueue[T] {
+	q := PriorityQueue[T]{
+		elements: make([]*priorityQueueNode[T], 0, capacity),
 	}
+
+	if !maximize {
+		q.compFunc = func(i, j int) bool {
+			return q.elements[i].Priority < q.elements[j].Priority
+		}
+	} else {
+		q.compFunc = func(i, j int) bool {
+			return q.elements[i].Priority > q.elements[j].Priority
+		}
+	}
+
+	return &q
 }
 
-// Push a new item onto the priority queue
-func (q *PriorityQueue[T, K]) Push(value T, priority K) {
-	node := priorityQueueNode[T, K]{
-		priority: priority,
-		data:     value,
+func (q *PriorityQueue[T]) Less(i, j int) bool {
+	return q.compFunc(i, j)
+}
+
+func (q *PriorityQueue[T]) Push(x any) {
+	item := x.(*priorityQueueNode[T])
+	item.Index = len(q.elements)
+	q.elements = append(q.elements, item)
+}
+
+// PushItem a new item onto the priority queue
+func (q *PriorityQueue[T]) PushItem(value T, priority int) {
+	node := priorityQueueNode[T]{
+		Priority: priority,
+		Data:     value,
 	}
-	q.data = append(q.data, &node)
-	q.heapifyUp(len(q.data) - 1)
+	heap.Push(q, &node)
 }
 
 // Get the current size of the priority queue
-func (q *PriorityQueue[T, K]) Size() int {
-	return len(q.data)
+func (q *PriorityQueue[T]) Len() int {
+	return len(q.elements)
 }
 
 // Retrieve the next item without popping it
-func (q *PriorityQueue[T, K]) Peek() (*T, error) {
-	if len(q.data) == 0 {
+func (q *PriorityQueue[T]) Peek() (*T, error) {
+	if len(q.elements) == 0 {
 		return nil, errors.New("No items are in the queue.")
 	}
-	return &q.data[0].data, nil
+	return &q.elements[0].Data, nil
+}
+
+func (q *PriorityQueue[T]) String() string {
+	parts := make([]string, len(q.elements))
+	for _, node := range q.elements {
+		parts = append(parts, fmt.Sprintf("%v:%v", node.Priority, node.Data))
+	}
+	str := strings.Join(parts, ",")
+	return fmt.Sprintf("PriorityQueue{%v}", str)
+}
+
+func (q *PriorityQueue[T]) Pop() any {
+	old := q.elements
+	n := len(old)
+	item := old[n-1]
+	item.Index = -1 // Mark as removed.
+	q.elements = old[0 : n-1]
+	return item
+}
+
+func (q PriorityQueue[T]) Swap(i, j int) {
+	q.elements[i], q.elements[j] = q.elements[j], q.elements[i]
+	q.elements[i].Index = i
+	q.elements[j].Index = j
 }
 
 // Pop the next item
-func (q *PriorityQueue[T, K]) Pop() (*T, error) {
-	if len(q.data) == 0 {
-		return nil, errors.New("Queue is empty")
-	}
-	head := q.data[0]
-	q.data[0] = q.data[len(q.data)-1]
-	q.data[len(q.data)-1] = nil
-
-	q.data = q.data[:len(q.data)-1]
-	q.heapifyDown(0)
-	return &head.data, nil
+func (pq *PriorityQueue[T]) PopItem() T {
+	item := heap.Pop(pq).(*priorityQueueNode[T])
+	return item.Data
 }
 
 // Heapify the current index up
-func (q *PriorityQueue[T, K]) heapifyUp(index int) {
+func (q *PriorityQueue[T]) heapifyUp(index int) {
 	if index == 0 {
 		return
 	}
-	currentNode := q.data[index]
+	currentNode := q.elements[index]
 	parentIndex := q.parent(index)
-	parentNode := q.data[parentIndex]
+	parentNode := q.elements[parentIndex]
 
-	if parentNode.priority <= currentNode.priority {
+	if parentNode.Priority <= currentNode.Priority {
 		return
 	}
-	q.data[index] = parentNode
-	q.data[parentIndex] = currentNode
+	q.elements[index] = parentNode
+	q.elements[parentIndex] = currentNode
 	q.heapifyUp(parentIndex)
 }
 
 // Heapify the current index down
-func (q *PriorityQueue[T, K]) heapifyDown(index int) {
+func (q *PriorityQueue[T]) heapifyDown(index int) {
 
-	if index >= len(q.data) {
+	if index >= len(q.elements) {
 		return
 	}
 
 	leftChildIndex := q.leftChild(index)
-	if leftChildIndex > len(q.data)-1 {
+	if leftChildIndex > len(q.elements)-1 {
 		return
 	}
 
 	rightChildIndex := q.rightChild(index)
-	currentNode := q.data[index]
-	leftNode := q.data[leftChildIndex]
+	currentNode := q.elements[index]
+	leftNode := q.elements[leftChildIndex]
 
-	if rightChildIndex < len(q.data) {
-		if leftNode.priority < currentNode.priority {
-			q.data[index] = leftNode
-			q.data[leftChildIndex] = currentNode
+	if rightChildIndex < len(q.elements) {
+		if leftNode.Priority < currentNode.Priority {
+			q.elements[index] = leftNode
+			q.elements[leftChildIndex] = currentNode
 			q.heapifyDown(leftChildIndex)
 		}
 	}
 
-	var rightNode *priorityQueueNode[T, K] = nil
-	if rightChildIndex < len(q.data) {
-		rightNode = q.data[rightChildIndex]
+	var rightNode *priorityQueueNode[T] = nil
+	if rightChildIndex < len(q.elements) {
+		rightNode = q.elements[rightChildIndex]
 	}
 
-	if (rightNode == nil || leftNode.priority < rightNode.priority) &&
-		leftNode.priority < currentNode.priority {
-		q.data[index] = leftNode
-		q.data[leftChildIndex] = currentNode
+	if (rightNode == nil || leftNode.Priority < rightNode.Priority) &&
+		leftNode.Priority < currentNode.Priority {
+		q.elements[index] = leftNode
+		q.elements[leftChildIndex] = currentNode
 		q.heapifyDown(leftChildIndex)
-	} else if rightNode != nil && rightNode.priority < currentNode.priority {
-		q.data[index] = rightNode
-		q.data[rightChildIndex] = currentNode
+	} else if rightNode != nil && rightNode.Priority < currentNode.Priority {
+		q.elements[index] = rightNode
+		q.elements[rightChildIndex] = currentNode
 		q.heapifyDown(rightChildIndex)
 	}
 }
 
-func (q *PriorityQueue[T, K]) parent(index int) int {
+func (q *PriorityQueue[T]) parent(index int) int {
 	return (index - 1) / 2
 }
 
-func (q *PriorityQueue[T, K]) leftChild(index int) int {
+func (q *PriorityQueue[T]) leftChild(index int) int {
 	return 2*index + 1
 }
 
-func (q *PriorityQueue[T, K]) rightChild(index int) int {
+func (q *PriorityQueue[T]) rightChild(index int) int {
 	return 2*index + 2
 }
--- algorithms/priorityqueue_test.go
+++ algorithms/priorityqueue_test.go
@@ -2,76 +2,130 @@ package algorithms_test
 
 import (
 	"fmt"
+	"math"
 	"math/rand/v2"
 	"testing"
 
 	"github.com/ymiseddy/go_tools/algorithms"
 )
 
-func popQueue(t *testing.T, q *algorithms.PriorityQueue[int, int]) {
-	prev := -1
-	for q.Size() > 0 {
-		val, err := q.Pop()
-		if err != nil {
-			panic(err)
-		}
-		if prev > *val {
-			t.Fatalf(fmt.Sprintf("Item popped out of order %d > %d", prev, *val))
+func PopExpectedMaximum(t *testing.T, q *algorithms.PriorityQueue[int], expected []int) {
+	expectedMap := make(map[int]bool)
+	for _, val := range expected {
+		expectedMap[val] = false
+	}
+
+	prevVal := math.MaxInt64
+	for q.Len() > 0 {
+		val := q.PopItem()
+
+		// Ensure val is in expected
+		_, ok := expectedMap[val]
+		if !ok {
+			t.Fatalf("Popped unexpected value %d", val)
 		}
 
-		prev = *val
+		if prevVal < val {
+			t.Fatalf(fmt.Sprintf("Item popped out of order %d > %d", prevVal, val))
+		}
+		expectedMap[val] = true
+
+		prevVal = val
+	}
+
+	for _, val := range expected {
+		if !expectedMap[val] {
+			t.Fatalf("Expected %d but not found", val)
+		}
+	}
+}
+
+func PopExpectedMinimum(t *testing.T, q *algorithms.PriorityQueue[int], expected []int) {
+	expectedMap := make(map[int]bool)
+	for _, val := range expected {
+		expectedMap[val] = false
+	}
+
+	prevVal := -1
+	for q.Len() > 0 {
+		val := q.PopItem()
+
+		// Ensure val is in expected
+		_, ok := expectedMap[val]
+		if !ok {
+			t.Fatalf("Popped unexpected value %d", val)
+		}
+
+		if prevVal > val {
+			t.Fatalf(fmt.Sprintf("Item popped out of order %d > %d", prevVal, val))
+		}
+		expectedMap[val] = true
+
+		prevVal = val
+	}
+
+	for _, val := range expected {
+		if !expectedMap[val] {
+			t.Fatalf("Expected %d but not found", val)
+		}
 	}
 }
 
 func TestPriorityQueue_PushInOrder_ShouldPopInOrder(t *testing.T) {
-	q := algorithms.NewPriorityQueue[int, int](100)
+	q := algorithms.NewMinCostPriorityQueue[int](100)
+	expectedItems := []int{}
+
 	for x := 0; x < 100; x++ {
-		q.Push(x, x)
+		expectedItems = append(expectedItems, x)
+		q.PushItem(x, x)
 	}
-	popQueue(t, &q)
+	PopExpectedMinimum(t, q, expectedItems)
+}
+
+func TestPriorityQueueMaximumCost_PushInOrder_ShouldPopInOrder(t *testing.T) {
+	q := algorithms.NewMaxCostPriorityQueue[int](100)
+	expectedItems := []int{}
+
+	for x := 0; x < 100; x++ {
+		expectedItems = append(expectedItems, x)
+		q.PushItem(x, x)
+	}
+	PopExpectedMaximum(t, q, expectedItems)
 }
 
 func TestPriorityQueue_PushOutOfOrder_ShouldPopInOrder(t *testing.T) {
-	q := algorithms.NewPriorityQueue[int, int](100)
-	for x := 0; x < 100; x++ {
-		q.Push(x, x)
+	q := algorithms.NewMinCostPriorityQueue[int](100)
+	expectedItems := []int{}
+	for x := 99; x >= 0; x-- {
+		expectedItems = append(expectedItems, x)
+		q.PushItem(x, x)
 	}
 
-	popQueue(t, &q)
+	PopExpectedMinimum(t, q, expectedItems)
 }
 
 func TestPriorityQueue_PushOutOfOrder_PeekShouldShowValueBeforePopping(t *testing.T) {
-	q := algorithms.NewPriorityQueue[int, int](100)
-	for x := 0; x < 100; x++ {
-		q.Push(x, x)
+	q := algorithms.NewMinCostPriorityQueue[int](10)
+	for x := 9; x >= 0; x-- {
+		q.PushItem(x, x)
+		t.Logf("Pushing %v", q.String())
 	}
 
-	for q.Size() > 0 {
+	for q.Len() > 0 {
 		peekVal, err := q.Peek()
 		if err != nil {
 			panic(err)
 		}
-		popVal, popErr := q.Pop()
-		if popErr != nil {
-			panic(popErr)
-		}
+		popVal := q.PopItem()
 
-		if *peekVal != *popVal {
+		if *peekVal != popVal {
 			t.Fatalf("Peek value mismatch expected %d got %d", peekVal, popVal)
 		}
 	}
 }
 
-func TestPriorityQueue_Pop_ShouldFailOnEmptyQueue(t *testing.T) {
-	q := algorithms.NewPriorityQueue[int, int](100)
-	_, err := q.Pop()
-	if err == nil {
-		t.Fatalf("Expected error.")
-	}
-}
-
 func TestPriorityQueue_Peek_ShouldFailOnEmptyQueue(t *testing.T) {
-	q := algorithms.NewPriorityQueue[int, int](100)
+	q := algorithms.NewMinCostPriorityQueue[int](100)
 	_, err := q.Peek()
 	if err == nil {
 		t.Fatalf("Expected error.")
@@ -79,23 +133,24 @@ func TestPriorityQueue_Peek_ShouldFailOnEmptyQueue(t *testing.T) {
 }
 
 func TestPriorityQueue_FuzzTesting_ShouldPopInOrder(t *testing.T) {
-	q := algorithms.NewPriorityQueue[int, int](100)
-
+	q := algorithms.NewMinCostPriorityQueue[int](100)
+	expectedItems := make([]int, 0, 100)
 	for x := 0; x < 100; x++ {
 		val := rand.IntN(1000)
-		q.Push(val, val)
+		expectedItems = append(expectedItems, val)
+		q.PushItem(val, val)
 	}
-	popQueue(t, &q)
+	PopExpectedMinimum(t, q, expectedItems)
 }
 
 func PriorityQueueRandom(count int) error {
-	q := algorithms.NewPriorityQueue[int, int](count)
+	q := algorithms.NewMinCostPriorityQueue[int](count)
 	for x := 0; x < count; x++ {
 		val := rand.IntN(1000)
-		q.Push(val, val)
+		q.PushItem(val, val)
 	}
 
-	for q.Size() > 0 {
+	for q.Len() > 0 {
 		q.Pop()
 	}
 	return nil
